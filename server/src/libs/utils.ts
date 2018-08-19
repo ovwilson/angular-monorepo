a13503863feb7cb598ed36f2cadff5f2001cbd2b
orlando <landough@Orlandos-iMac.fios-router.home>
2018-08-19 15:05:20 -0400

    added ease in east out and load animations

--- server/src/libs/utils.ts
+++ server/src/libs/utils.ts
@@ -68,7 +68,7 @@ export const properCase = (str: string): string => {
         return '';
     }
     return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
-}
+};
 
 export const convertName = (str: string): string => {
     const name = str.slice(1, str.length - 1);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getAnimations } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: getAnimations()
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-monorepo';
+  animationState = 'start';
+
+  ngOnInit() {
+    this.animationState = 'start';
+    setTimeout(() => {
+      this.animationState = 'end';
+    }, 4000);
+  }
+
+  animateToggle() {
+    this.animationState = (this.animationState === 'start' ? 'end' : 'start');
+  }
 }
