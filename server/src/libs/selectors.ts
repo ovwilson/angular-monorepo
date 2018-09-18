3e79c20a06420a66de3267670b179149ee4107b9
landough <orlando_wilson@msn.com>
2018-09-18 09:57:05 -0400

    update to transform sections

--- server/src/libs/selectors.ts
+++ server/src/libs/selectors.ts
@@ -5,6 +5,10 @@ interface SectionDictionary {
     [section: string]: ISectionSchema;
 }
 
+interface FieldDictionary {
+    [field: string]: IFieldSchema;
+}
+
 export const createSectionDictionary = (sections: ISectionSchema[]) => {
     let dictionary: SectionDictionary = {};
     sections.map(val => dictionary = Object.assign({}, dictionary,
@@ -18,4 +22,31 @@ export const createSectionArray = (dictionary: SectionDictionary): ISectionSchem
     key.map(k => { sections = [...sections, Object.assign({}, dictionary[k])]; });
     return sections;
 };
+
+export const transformSections = (dictionary: SectionDictionary, sections: ISectionSchema[]): ISectionSchema[] => {
+    sections.map(section => dictionary[section.label] ? dictionary[section.label] = mergeSection(dictionary[section.label], section) :
+        dictionary = appendSection(dictionary, section));
+    return createSectionArray(dictionary);
+};
+
+export const mergeSection = (origSection: ISectionSchema, section: ISectionSchema): ISectionSchema => {
+    return Object.assign({}, origSection, { fields: mergeFields(origSection.fields, section.fields) });
+};
+
+export const mergeFields = (origFields: IFieldSchema[], fields: IFieldSchema[]): IFieldSchema[] => {
+    const fieldDictionary = createFieldDictionary(fields);
+    return origFields.map(field => fieldDictionary[field.name] ? Object.assign({}, fieldDictionary[field.name]) : field);
+};
+
+export const appendSection = (dictionary: SectionDictionary, section: ISectionSchema): SectionDictionary => {
+    dictionary[section.label] = section;
+    return dictionary;
+};
+
+export const createFieldDictionary = (fields: IFieldSchema[]): FieldDictionary{
+    const dictionary: FieldDictionary = {};
+    fields.map(field => dictionary[field.name] = field);
+    return dictionary;
+};
+
 
